0fb954522a76decdac0cdfdabf806ef49e742353
Jinhyoung Lee <jhcode0@naver.com>
2023-03-08 18:35:30 +0900

    'Routes' instead of 'Switch'

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,16 @@
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./screen/Home";
+import About from "./screen/About";
 
 function Router(){
     return(
         <BrowserRouter>
             <Header/>
-        
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+            </Routes>
         </BrowserRouter>
     );
 }
