cb8918b3d0368e1a907b9f39150474e2984e18ac
Jinhyoung Lee <jhcode0@naver.com>
2023-03-09 13:03:21 +0900

    useNavigate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 function Header() {
+    const navigate = useNavigate();
+    const onAboutClick = ()=> {
+        navigate("/about");
+    }
     return (
         <header>
             <h1>로고입니다</h1>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/About">About</Link></li>
+                <li><Link to="/about">About</Link></li>
             </ul>
         </header>
        
